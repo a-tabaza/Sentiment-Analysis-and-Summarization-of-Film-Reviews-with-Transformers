4f8154b9ffc2787f202abc77a706fa9ac7ee99eb
Abdulrahman Tabaza <84684514+a-tabaza@users.noreply.github.com>
2022-10-15 21:48:45 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,15 @@
+# Sentiment Analyisis for Ichi the Killer (2001)
+## _Truly vile, yet critically lauded in online media criticism circles._
+
+The context for this project is kinda of simple, I get this unbelievable urge to rage sometimes after I see a movie that is considered part of the whole extreme wave of new films, don't know the correct term for it, but extreme cinema in general.
+This time I chose to run some basic sentiment analysis on this Ichi the Killer. A movie I despised with my whole heart.
+
+I used Python to scrape and compile the dataset, R for sentiment analysis and visualization.
+
+Two take aways:
+- I liked the word cloud that I was able to generate
+- I really like how I was able to scrape IMDb reviews ESPECIALLY after I found out the IMDb datasets sell for $50-$400k
+
+I used R-bloggers and Analytics Vidhya as resources, linked here: 
+- https://www.analyticsvidhya.com/blog/2022/07/scraping-imdb-reviews-in-python-using-selenium/
+- https://www.r-bloggers.com/2021/05/sentiment-analysis-in-r-3/
---
+++ sentiment_analysis.R
@@ -0,0 +1,83 @@
+# sentiment analysis || sources: https://www.r-bloggers.com/2021/05/sentiment-analysis-in-r-3/
+
+#load data
+data <- read.csv("C:/Users/User/Desktop/ichi the killer sentiment analysis/ichi_the_killer_imdb_reviews.csv", header = T)
+
+
+# build corpus
+library(tm)
+corpus <- iconv(data$Review)
+corpus <- Corpus(VectorSource(corpus))
+inspect(corpus[1:5])
+
+# clean text
+corpus <- tm_map(corpus, tolower)
+inspect(corpus[1:5])
+corpus <- tm_map(corpus, removePunctuation)
+inspect(corpus[1:5])
+corpus <- tm_map(corpus, removeNumbers)
+inspect(corpus[1:5])
+cleanset <- tm_map(corpus, removeWords, stopwords('english'))
+inspect(cleanset[1:5])
+
+
+cleanset <- tm_map(cleanset, removeWords, c('ichi', 'film', 'movie','movies', 'killer', 'miikes','miike'))
+
+
+# cleanset <- tm_map(cleanset, stemDocument)
+inspect(cleanset[1:5])
+
+# term document matrix
+tdm <- TermDocumentMatrix(cleanset)
+tdm <- as.matrix(tdm)
+tdm[1:10, 1:20]
+
+
+# bar plot
+w <- rowSums(tdm)
+w <- subset(w, w>=25)
+barplot(w,
+        las = 2,
+        col = rainbow(50))
+
+# word cloud
+library(wordcloud)
+w <- sort(rowSums(tdm), decreasing = TRUE)
+set.seed(222)
+wordcloud(words = names(w),
+          freq = w,
+          max.words = 150,
+          random.order = F,
+          min.freq = 5,
+          colors = brewer.pal(8, 'Dark2'),
+          scale = c(5, 0.3),
+          rot.per = 0.7)
+
+library(wordcloud2)
+w <- data.frame(names(w), w)
+colnames(w) <- c('word', 'freq')
+wordcloud2(w,
+           size = 0.7,
+           shape = 'triangle',
+           rotateRatio = 0.5,
+           minSize = 1)
+
+# sentiment analysis
+library(pacman)
+p_load(syuzhet,lubridate,ggplot2,scales,reshape2,dplyr)
+library(syuzhet)
+library(lubridate)
+library(ggplot2)
+library(scales)
+library(reshape2)
+library(dplyr)
+
+reviews <- iconv(data$Review)
+s <- get_nrc_sentiment(reviews)
+head(s)
+
+barplot(colSums(s),
+        las = 2,
+        col = rainbow(10),
+        ylab = 'Count',
+        main = 'Sentiment Scores for Reviews')
